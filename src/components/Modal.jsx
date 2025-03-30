e0469bfdc7e380a21d24aa5c965fe8225bb64482
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-31 00:53:42 +0530

    ⌃ enhance Header and Modal components with motion animations...

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,20 @@
 import { createPortal } from "react-dom";
+import { motion } from "motion/react";
 
 export default function Modal({ title, children, onClose }) {
    return createPortal(
       <>
          <div className="backdrop" onClick={onClose} />
-         <dialog open className="modal">
+         <motion.dialog
+            open
+            className="modal"
+            initial={{ opacity: 0, scale: 0.2, y: 600 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 0.1, y: 800 }}
+         >
             <h2>{title}</h2>
             {children}
-         </dialog>
+         </motion.dialog>
       </>,
       document.getElementById("modal")
    );
